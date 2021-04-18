b58f51df004db662296857d83616074dfddf61c9
Andrew Pitt <apitt@redhat.com>
2021-04-18 10:59:55 -0400

    Fixed data imports.

--- src/main/resources/data-postgresql.sql
+++ src/main/resources/data-postgresql.sql
@@ -1,2 +1,2 @@
-INSERT INTO books (isbn, title, author, publisher, genre) VALUES ('1234', 'The Shining', 'Stephen King', 'Random House', 'Horror');
-INSERT INTO books (isbn, title, author, publisher, genre) VALUES ('3456', 'The Name of the Wind', 'Patrick Rothfuss', 'Columbia', 'Fantasy');
+INSERT INTO books (isbn, title, author, publisher, genre) VALUES ('1234', 'The Shining', 'Stephen King', 'Random House', 'Horror') ON CONFLICT (isbn) DO NOTHING;
+INSERT INTO books (isbn, title, author, publisher, genre) VALUES ('3456', 'The Name of the Wind', 'Patrick Rothfuss', 'Columbia', 'Fantasy') ON CONFLICT (isbn) DO NOTHING;
